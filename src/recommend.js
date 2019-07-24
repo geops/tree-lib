4e71a3981968484c9c0bc4137310e5a6f0bd3298
Ang Sherpa <angsherpa232@gmail.com>
2019-07-24 12:45:08 +0200

    future flag boolean type test running.

--- src/recommend.js
+++ src/recommend.js
@@ -2,6 +2,9 @@ import recommendations from '../data/recommendations.json';
 import types from '../data/types.json';
 
 function recommendTreeSpecies(forestType) {
+  if (types.forestType.find(v => v.code === forestType) === undefined) {
+    throw new Error(`${forestType} is not a valid forest Type`);
+  }
   const [, treeSpecies] = Object.entries(recommendations).find(
     t => t[0] === forestType,
   );
@@ -14,45 +17,39 @@ const concatTreeSpecies = (treeSp1, treeSp2) =>
 const filterTreeSpecies = (treeSp1, treeSp2) =>
   treeSp2.filter(tree => !treeSp1.includes(tree));
 
-function validateForestType(forestType) {
-  if (types.forestType.find(v => v.code === forestType) === undefined) {
-    throw new Error(`${forestType} is not a valid forest Type`);
-  }
-}
-
 function recommend(forestType1, forestType2, future) {
-  let result;
   if (!forestType1) {
     throw new Error(
       `at least one forest type is required to get the recommendation of tree species`,
     );
   }
 
-  if (forestType1) {
-    validateForestType(forestType1);
-    result = recommendTreeSpecies(forestType1);
+  if (future && typeof future !== 'boolean') {
+    throw new Error(`expected boolean type for future flag`);
   }
 
+  let result;
+
+  result = recommendTreeSpecies(forestType1);
+
   const { one, two, three } = result;
-  if (forestType1 && forestType2 && future) {
+  if (forestType2) {
     const { one: one2, two: two2, three: three2 } = recommendTreeSpecies(
       forestType2,
     );
-    result = {
-      one: filterTreeSpecies(one, one2),
-      two: filterTreeSpecies(two, two2),
-      three: filterTreeSpecies(three, three2),
-    };
-  } else if (forestType1 && forestType2) {
-    validateForestType(forestType2);
-    const { one: one2, two: two2, three: three2 } = recommendTreeSpecies(
-      forestType2,
-    );
-    result = {
-      one: concatTreeSpecies(one, one2),
-      two: concatTreeSpecies(two, two2),
-      three: concatTreeSpecies(three, three2),
-    };
+    if (future) {
+      result = {
+        one: filterTreeSpecies(one, one2),
+        two: filterTreeSpecies(two, two2),
+        three: filterTreeSpecies(three, three2),
+      };
+    } else {
+      result = {
+        one: concatTreeSpecies(one, one2),
+        two: concatTreeSpecies(two, two2),
+        three: concatTreeSpecies(three, three2),
+      };
+    }
   }
 
   return result;
--- src/recommend.test.js
+++ src/recommend.test.js
@@ -18,10 +18,16 @@ describe('Test for input values', () => {
       `963D is not a valid forest Type`,
     );
   });
+
+  test('invalid argument type for future flag', () => {
+    expect(() => recommend('60', '963D')).toThrowError(
+      `963D is not a valid forest Type`,
+    );
+  });
 });
 
 describe('Test for output values', () => {
-  test('valid single forestType recommendations', () => {
+  test('valid single forest Type recommendations', () => {
     expect(recommend('60')).toStrictEqual({
       one: [302800],
       three: [800, 25400, 60400, 60500, 227200, 363700],
@@ -29,7 +35,7 @@ describe('Test for output values', () => {
     });
   });
 
-  test('valid multiple forestType recommendations', () => {
+  test('valid multiple forest Type recommendations', () => {
     expect(recommend('60', '50')).toStrictEqual({
       one: [302800, 100],
       two: [402300, 800],
@@ -48,11 +54,9 @@ describe('Test for output values', () => {
     });
   });
 
-  test('valid multiple forestType recommendations with future flag', () => {
-    expect(recommend('60', '50', true)).toStrictEqual({
-      one: [100],
-      two: [800],
-      three: [25300, 174200, 317500, 402200],
-    });
+  test('invalid input type of future flag', () => {
+    expect(() => recommend('60', '50', 56)).toThrowError(
+      `expected boolean type for future flag`,
+    );
   });
 });
